e1b6f3295f49bc3d1fc668defef9ab6a06fb2e58
JackPu <kakashjack@gmail.com>
2017-02-06 19:49:47 +0800

    add demo folder

--- src/lib/drag.js
+++ src/lib/drag.js
@@ -1,26 +1,27 @@
 import helper from './helper';
 
 const isMobile = helper.isMobile;
-let moves, stopM;
+let moves;
+let stopM;
 export default class Drag {
   constructor($el, $container, e) {
     const self = this;
     this.el = $el;
     this.container = $container;
     this.coor = {
-      x: (isMobile ? e.touches[0]['clientX'] : e.clientX) - $el.offsetLeft - $el.parentElement.offsetLeft - document.getElementsByClassName('image-aside')[0].offsetLeft
-      , y: (isMobile ? e.touches[0]['clientY'] : e.clientY) - $el.offsetTop - $el.parentElement.offsetTop - document.getElementsByClassName('image-aside')[0].offsetTop
-      , posX: isMobile ? e.touches[0]['clientX'] : e.clientX
-      , posy: isMobile ? e.touches[0]['clientY'] : e.clientY
-      , maxLeft: parseInt(this.container.style.width) - parseInt(this.el.style.width)
-      , maxTop: parseInt(this.container.style.height) - parseInt(this.el.style.height)
-    , };
-    moves = function (e) {
-      self.move(e);
-    }
-    stopM = function (e) {
-      self.stopMove(e);
-    }
+      x: (isMobile ? e.touches[0]['clientX'] : e.clientX) - $el.offsetLeft - $el.parentElement.offsetLeft - document.getElementsByClassName('image-aside')[0].offsetLeft,
+      y: (isMobile ? e.touches[0]['clientY'] : e.clientY) - $el.offsetTop - $el.parentElement.offsetTop - document.getElementsByClassName('image-aside')[0].offsetTop,
+      posX: isMobile ? e.touches[0]['clientX'] : e.clientX,
+      posy: isMobile ? e.touches[0]['clientY'] : e.clientY,
+      maxLeft: parseInt(this.container.style.width) - parseInt(this.el.style.width),
+      maxTop: parseInt(this.container.style.height) - parseInt(this.el.style.height),
+    };
+    moves = function (ev) {
+      self.move(ev);
+    };
+    stopM = function (ev) {
+      self.stopMove(ev);
+    };
     if (isMobile) {
       document.addEventListener('touchmove', moves, false);
       document.addEventListener('touchend', stopM, false);
@@ -35,10 +36,10 @@ export default class Drag {
     }
     // this.coor.posX = isMobile ? e.changedTouches[0]['clientX']:e.clientX;
     // this.coor.posY = isMobile ? e.changedTouches[0]['clientY']:e.clientY;
-    var aa = isMobile ? e.changedTouches[0]['clientX'] : e.clientX;
-    var bb = isMobile ? e.changedTouches[0]['clientY'] : e.clientY;
-    var newPosX = aa - this.el.parentElement.offsetLeft - document.getElementsByClassName('image-aside')[0].offsetLeft - this.coor.x;
-    var newPosY = bb - this.el.parentElement.offsetTop - document.getElementsByClassName('image-aside')[0].offsetTop - this.coor.y;
+    const aa = isMobile ? e.changedTouches[0]['clientX'] : e.clientX;
+    const bb = isMobile ? e.changedTouches[0]['clientY'] : e.clientY;
+    let newPosX = aa - this.el.parentElement.offsetLeft - document.getElementsByClassName('image-aside')[0].offsetLeft - this.coor.x;
+    let newPosY = bb - this.el.parentElement.offsetTop - document.getElementsByClassName('image-aside')[0].offsetTop - this.coor.y;
     if (newPosX <= 0) {
       newPosX = 0;
     }
@@ -55,7 +56,6 @@ export default class Drag {
     this.el.style.top = (newPosY) + 'px';
   }
   stopMove() {
-    var self = this;
     this.el = null;
     if (isMobile) {
       document.removeEventListener('touchmove', moves, false);
@@ -65,4 +65,4 @@ export default class Drag {
     document.removeEventListener('mousemove', moves, false);
     document.removeEventListener('mouseup', stopM, false);
   }
-};
+}
--- src/lib/resize.js
+++ src/lib/resize.js
@@ -5,9 +5,12 @@
  * e events
  **/
 import helper from './helper';
+
 let drags;
 let stopD;
 const isMobile = helper.isMobile;
+const W = document.body.offsetWidth;
+const H = document.body.offsetHeight;
 export default class Resize {
   constructor($el, $container, ratio, e) {
     e.stopPropagation();
@@ -22,113 +25,91 @@ export default class Resize {
     this.splitY = ratio.split(':')[1];
     this.el = $el;
     this.container = $container;
-    drags = function (e) {
-      self.drag(e);
+    drags = function (ev) {
+      self.drag(ev);
     };
-    stopD = function(e) {
-      self.stopDrag(e);
-    }
-    if(isMobile) {
+    stopD = function (ev) {
+      self.stopDrag(ev);
+    };
+    if (isMobile) {
       document.addEventListener('touchmove', drags, false);
-      document.addEventListener('touchend', stopD, false)
+      document.addEventListener('touchend', stopD, false);
     }
     document.addEventListener('mousemove', drags, false);
     document.addEventListener('mouseup', stopD, false);
-
   }
 
   drag(e) {
-    if(!this.el) {
+    if (!this.el) {
       return;
     }
-    let $dotBox = document.querySelector('.g-crop-image-principal');
-    let $topH = document.querySelector('.info-aside');
-    let $halfX = parseInt(document.body.offsetWidth) - parseInt(window.getComputedStyle($dotBox).width);
-    let $halfY = parseInt(document.body.offsetHeight) - parseInt(window.getComputedStyle($dotBox).height) - parseInt(window.getComputedStyle($topH).height);
-    let resetX = isMobile ? e.changedTouches[0]['clientX'] : e.clientX;
-    let resetY = isMobile ? e.changedTouches[0]['clientY'] : e.clientY;
+    const $dotBox = document.querySelector('.g-crop-image-principal');
+    const dotBoxW = window.getComputedStyle($dotBox).width;
+    const dotBoxH = window.getComputedStyle($dotBox).height;
+    const $topH = document.querySelector('.info-aside');
+    const $halfX = W - dotBoxW;
+    const $halfY = H - dotBoxH - window.getComputedStyle($topH).height;
+    const resetX = isMobile ? e.changedTouches[0]['clientX'] : e.clientX;
+    const resetY = isMobile ? e.changedTouches[0]['clientY'] : e.clientY;
 
     if (this.splitX > this.splitY) {
-      if (parseInt(resetX) >= ($halfX / 2) + parseInt(window.getComputedStyle($dotBox).width)) {
-        return;
-      } else {
-        if (parseInt(this.el.offsetWidth) >= parseInt(window.getComputedStyle($dotBox).width)) {
+      if (parseInt(resetX) <= ($halfX / 2) + dotBoxW) {
+        if (parseInt(this.el.offsetWidth) >= dotBoxW) {
           this.el.style.width = window.getComputedStyle($dotBox).width;
-        };
-        this.el.style.width = (this.coor.w + (isMobile ? e.changedTouches[0]['clientX']:e.clientX) - this.coor.x) + 'px';
+        }
+        this.el.style.width = (this.coor.w + (isMobile ? e.changedTouches[0]['clientX'] : e.clientX) - this.coor.x) + 'px';
         this.el.style.height = parseInt(this.el.offsetWidth) * (this.splitY / this.splitX) + 'px';
-        //限制拖拉的范围在图片内
-        if ( parseInt(window.getComputedStyle($dotBox).width) >  parseInt(window.getComputedStyle($dotBox).height)) {
+        if (dotBoxW > dotBoxH) {
           if (parseInt(this.el.offsetWidth) >= parseInt(window.getComputedStyle($dotBox).height)) {
             this.el.style.height = window.getComputedStyle($dotBox).height;
             this.el.style.width = parseInt(window.getComputedStyle($dotBox).height) * (this.splitX / this.splitY) + 'px';
-            return;
-          };
-        } else if(parseInt(window.getComputedStyle($dotBox).width) <  parseInt(window.getComputedStyle($dotBox).height)) {
-          if (parseInt(this.el.offsetWidth) >= parseInt(window.getComputedStyle($dotBox).width)) {
-            this.el.style.width = window.getComputedStyle($dotBox).width;
-            this.el.style.height = parseInt(window.getComputedStyle($dotBox).width) * (this.splitY / this.splitX) + 'px';
-            return;
           }
-        } else {
-          if (parseInt(this.el.offsetHeight) >= parseInt(window.getComputedStyle($dotBox).width)) {
+        } else if (dotBoxW < dotBoxH) {
+          if (parseInt(this.el.offsetWidth) >= dotBoxW) {
             this.el.style.width = window.getComputedStyle($dotBox).width;
-            this.el.style.height = parseInt(window.getComputedStyle($dotBox).width) * (this.splitY / this.splitX) + 'px';
-            return;
+            this.el.style.height = dotBoxW * (this.splitY / this.splitX) + 'px';
           }
+        } else if (parseInt(this.el.offsetHeight) >= dotBoxW) {
+          this.el.style.width = dotBoxW;
+          this.el.style.height = dotBoxW * (this.splitY / this.splitX) + 'px';
         }
       }
-    } else if(this.splitX < this.splitY){
-      if (parseInt(resetY) >= ($halfY / 2) + parseInt(window.getComputedStyle($dotBox).height) + parseInt(window.getComputedStyle($topH).height)) {
-        return;
-      } else {
-        this.el.style.height = (this.coor.h + (isMobile ? e.changedTouches[0]['clientY']:e.clientY) - this.coor.y) + 'px';
+    } else if (this.splitX < this.splitY) {
+      if (parseInt(resetY) <= ($halfY / 2) + dotBoxH + window.getComputedStyle($topH).height) {
+        this.el.style.height = (this.coor.h + (isMobile ? e.changedTouches[0]['clientY'] : e.clientY) - this.coor.y) + 'px';
         this.el.style.width = parseInt(this.el.style.height) * (this.splitX / this.splitY) + 'px';
-        //限制拖拉的范围在图片内
-        if ( parseInt(window.getComputedStyle($dotBox).width) >  parseInt(window.getComputedStyle($dotBox).height)) {
-          if (parseInt(this.el.offsetHeight) >= parseInt(window.getComputedStyle($dotBox).height)) {
-            this.el.style.height = window.getComputedStyle($dotBox).height;
-            this.el.style.width = parseInt(window.getComputedStyle($dotBox).height) * (this.splitX / this.splitY) + 'px';
-            return;
-          };
-        } else if(parseInt(window.getComputedStyle($dotBox).width) <  parseInt(window.getComputedStyle($dotBox).height)) {
-          if (parseInt(this.el.offsetWidth) >= parseInt(window.getComputedStyle($dotBox).width)) {
-            this.el.style.width = window.getComputedStyle($dotBox).width;
-            this.el.style.height = parseInt(window.getComputedStyle($dotBox).width) * (this.splitY / this.splitX) + 'px';
-            return;
+        // 限制拖拉的范围在图片内
+        if (dotBoxW > dotBoxH) {
+          if (parseInt(this.el.offsetHeight) >= dotBoxH) {
+            this.el.style.height = dotBoxH;
+            this.el.style.width = dotBoxH * (this.splitX / this.splitY) + 'px';
           }
-        } else {
-          if (parseInt(this.el.offsetWidth) >= parseInt(window.getComputedStyle($dotBox).width)) {
-            this.el.style.width = window.getComputedStyle($dotBox).width;
-            this.el.style.height = parseInt(window.getComputedStyle($dotBox).width) * (this.splitY / this.splitX) + 'px';
-            return;
+        } else if (dotBoxW < dotBoxH) {
+          if (parseInt(this.el.offsetWidth) >= dotBoxW) {
+            this.el.style.width = dotBoxW;
+            this.el.style.height = dotBoxW * (this.splitY / this.splitX) + 'px';
           }
+        } else if (parseInt(this.el.offsetWidth) >= dotBoxW) {
+          this.el.style.width = dotBoxW;
+          this.el.style.height = dotBoxW * (this.splitY / this.splitX) + 'px';
         }
       }
-    } else {
-      if (parseInt(resetX) >= ($halfX / 2) + parseInt(window.getComputedStyle($dotBox).width)) {
-        //现在拖拉范围不准超过某一边的范围
-        return;
-      } else {
-
-        this.el.style.width = (this.coor.w + (isMobile ? e.changedTouches[0]['clientX']:e.clientX) - this.coor.x) + 'px';
-        this.el.style.height = this.el.style.width;
-        //限制拖拉的范围在图片内
-        if ( parseInt(window.getComputedStyle($dotBox).width) >  parseInt(window.getComputedStyle($dotBox).height)) {
-          if (parseInt(this.el.offsetHeight) >= parseInt(window.getComputedStyle($dotBox).height)) {
-            this.el.style.height = window.getComputedStyle($dotBox).height;
-            this.el.style.width = window.getComputedStyle($dotBox).height;
-          };
-        } else if(parseInt(window.getComputedStyle($dotBox).width) <  parseInt(window.getComputedStyle($dotBox).height)) {
-          if (parseInt(this.el.offsetWidth) >= parseInt(window.getComputedStyle($dotBox).width)) {
-            this.el.style.width = window.getComputedStyle($dotBox).width;
-            this.el.style.height = window.getComputedStyle($dotBox).width;
-          }
-        } else {
-          if (parseInt(this.el.offsetWidth) >= parseInt(window.getComputedStyle($dotBox).width)) {
-            this.el.style.width = this.el.style.height = window.getComputedStyle($dotBox).width;
-          }
+    } else if (parseInt(resetX) <= ($halfX / 2) + dotBoxW) {
+      this.el.style.width = (this.coor.w + (isMobile ? e.changedTouches[0]['clientX'] : e.clientX) - this.coor.x) + 'px';
+      this.el.style.height = this.el.style.width;
+      // 限制拖拉的范围在图片内
+      if (dotBoxW > dotBoxH) {
+        if (parseInt(this.el.offsetHeight) >= dotBoxH) {
+          this.el.style.height = dotBoxH;
+          this.el.style.width = dotBoxH;
         }
+      } else if (dotBoxW < dotBoxH) {
+        if (parseInt(this.el.offsetWidth) >= dotBoxW) {
+          this.el.style.width = dotBoxW;
+          this.el.style.height = dotBoxW;
+        }
+      } else if (parseInt(this.el.offsetWidth) >= dotBoxW) {
+        this.el.style.width = this.el.style.height = dotBoxW;
       }
     }
     // if(isMobile) {
@@ -138,14 +119,13 @@ export default class Resize {
     // }
   }
 
-  stopDrag(e) {
+  stopDrag() {
     this.el = null;
-    if(isMobile) {
-      document.removeEventListener('touchmove',drags,false);
-      document.removeEventListener('touchend',stopD,false);
+    if (isMobile) {
+      document.removeEventListener('touchmove', drags, false);
+      document.removeEventListener('touchend', stopD, false);
     }
-    document.removeEventListener('mousemove',drags,false);
-    document.removeEventListener('mouseup',stopD,false);
-    //this.container = null;
+    document.removeEventListener('mousemove', drags, false);
+    document.removeEventListener('mouseup', stopD, false);
   }
-};
+}
