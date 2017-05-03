0d729ea0ae3ca93833cc4e7ef6af0e7621c64296
JackPu <kakashjack@gmail.com>
2017-05-03 17:54:42 +0800

    update xhr for cross domain

--- src/lib/xhr.js
+++ src/lib/xhr.js
@@ -79,6 +79,7 @@ module.exports = function (method, url, headers, data, callback, err, isBinary) 
   if (isBinary) {
     return r.sendAsBinary(data);
   }
+  r.withCredentials = true;
   r.send(data);
   return r;
   // Headers are returned as a string
