5965ea847d285461e470c80cd78014a36c32982f
limdblur <limd808@gmail.com>
2017-11-03 14:37:54 +0800

    Update canvas-helper.js
    format

--- src/lib/canvas-helper.js
+++ src/lib/canvas-helper.js
@@ -4,10 +4,10 @@
 **/
 
 export default {
-  _getImageType(str) {
+  _getImageType (str) {
     let mimeType = 'image/jpeg';
     const outputType = str.match(/(image\/[\w]+)\.*/)[0];
-    if (typeof outputType !== 'undefined'){
+    if (typeof outputType !== 'undefined') {
       mimeType = outputType;
     }
     return mimeType;
@@ -17,14 +17,14 @@ export default {
       const reader = new FileReader();
       const self = this;
       (function (file) {
-        reader.onload = (function(event) {
+        reader.onload = (function (event) {
           const image = new Image();
           image.src = event.target.result;
-          image.onload = function() {
+          image.onload = function () {
             const mimeType = self._getImageType(file.type);
             const cvs = self._getCanvas(image.naturalWidth, image.naturalHeight);
-            const ctx = cvs.getContext("2d").drawImage(image, 0, 0);
-            const newImageData = cvs.toDataURL(mimeType, quality/100);
+            const ctx = cvs.getContext('2d').drawImage(image, 0, 0);
+            const newImageData = cvs.toDataURL(mimeType, quality / 100);
             file['base64Code'] = newImageData;
             callback(file);
           }
@@ -36,8 +36,8 @@ export default {
   /**
   * crop image via canvas and generate data
   **/
-  crop(image, options, callback) {
-    const checkNumber = function(num) {
+  crop (image, options, callback) {
+    const checkNumber = function (num) {
       return (typeof num === 'number');
     };
     // check crop options
@@ -47,7 +47,7 @@ export default {
         options.toCropImgH > 0) {
       let w = options.toCropImgW;
       let h = options.toCropImgH;
-      if(options.maxWidth && options.maxWidth < w) {
+      if (options.maxWidth && options.maxWidth < w) {
         w = options.maxWidth;
         h = options.toCropImgH * w / options.toCropImgW;
       }
@@ -55,71 +55,67 @@ export default {
         h = options.maxHeight
       }
       const cvs = this._getCanvas(w, h);
-      const ctx = cvs.getContext('2d').drawImage(image, options.toCropImgX, options.toCropImgY, options.toCropImgW, options.toCropImgH, 0 , 0, w, h);
+      const ctx = cvs.getContext('2d').drawImage(image, options.toCropImgX, options.toCropImgY, options.toCropImgW, options.toCropImgH, 0, 0, w, h);
       const mimeType = this._getImageType(image.src);
-      const data = cvs.toDataURL(mimeType, options.compress/100);
+      const data = cvs.toDataURL(mimeType, options.compress / 100);
       callback(data);
     }
   },
-
   /**
   * init image for reset size and rotation
   **/
-  init(src, callback) {
+  init (src, callback) {
       let image = new Image();
       image.src = src;
       var self = this;
-      image.onload = function() {
+      image.onload = function () {
         var mimeType = self._getImageType(image.src);
         var cvs = self._getCanvas(image.naturalWidth, image.naturalHeight);
-        var ctx = cvs.getContext("2d");
+        var ctx = cvs.getContext('2d');
         ctx.drawImage(image, 0, 0);
         var newImageData = cvs.toDataURL(mimeType, 100);
         callback(newImageData);
       }
   },
-
   /**
   * rotate image via canvas
   **/
-  rotate(imageData, direction, callback) {
+  rotate (imageData, direction, callback) {
       let image = new Image();
       image.src = imageData.src;
       var self = this;
-      image.onload = function() {
+      image.onload = function () {
         var mimeType = self._getImageType(image.src);
         var cvs = self._getCanvas(image.naturalHeight, image.naturalWidth);
-        var ctx = cvs.getContext("2d");
-        if (direction == 1) {
-            ctx.rotate(90 * Math.PI / 180);
-            ctx.translate(0, -cvs.width);
+        var ctx = cvs.getContext('2d');
+        if (direction === 1) {
+          ctx.rotate(90 * Math.PI / 180);
+          ctx.translate(0, -cvs.width);
         } else {
-            ctx.rotate(-90 * Math.PI / 180);
-            ctx.translate(-cvs.height, 0);
+          ctx.rotate(-90 * Math.PI / 180);
+          ctx.translate(-cvs.height, 0);
         }
         ctx.drawImage(image, 0, 0);
         var newImageData = cvs.toDataURL(mimeType, 100);
         callback(newImageData);
       }
   },
-
-  resize(image, options, callback) {
-    const checkNumber = function(num) {
+  resize (image, options, callback) {
+    const checkNumber = function (num) {
       return (typeof num === 'number');
     };
     if (checkNumber(options.toCropImgX) && checkNumber(options.toCropImgY) && options.toCropImgW > 0 && options.toCropImgH > 0) {
-      const w = options.toCropImgW  * options.imgChangeRatio;
+      const w = options.toCropImgW * options.imgChangeRatio;
       const h = options.toCropImgH * options.imgChangeRatio;
       const cvs = this._getCanvas(w, h);
-      const ctx = cvs.getContext('2d').drawImage(image, 0, 0, options.toCropImgW, options.toCropImgH, 0, 0, w , h);
+      const ctx = cvs.getContext('2d').drawImage(image, 0, 0, options.toCropImgW, options.toCropImgH, 0, 0, w, h);
       const mimeType = this._getImageType(image.src);
       const data = cvs.toDataURL(mimeType, options.compress / 100);
       callback(data);
     }
   },
-
-  rotate2(src, degrees, callback) {
-    this._loadImage(src, (image) => {
+  rotate2 (src, degrees, callback) {
+    this._loadImage(src, image => {
       let w = image.naturalWidth;
       let h = image.naturalHeight;
       const canvasWidth = Math.max(w, h);
@@ -156,8 +152,7 @@ export default {
       callback(data, w, h);
     });
   },
-
-  _loadImage(data, callback) {
+  _loadImage (data, callback) {
     const image = new Image();
     image.src = data;
     image.onload = function () {
@@ -167,12 +162,10 @@ export default {
       console.log('Error: image error!');
     };
   },
-
-  _getCanvas(width, height) {
+  _getCanvas (width, height) {
     const canvas = document.createElement('canvas');
     canvas.width = width;
     canvas.height = height;
     return canvas;
-  },
-
+  }
 };
